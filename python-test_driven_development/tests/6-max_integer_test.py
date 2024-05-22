69dd8cb9a945dd10181880bfde528aedb2c2c652
OussemaTurki0 <oussematurki0@gmail.com>
2024-05-22 02:46:05 +0200

    commit

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,19 +1,39 @@
-#!/usr/bin/python3
-"""Unittest for max_integer([..])
+#!/usr/bin/python
 """
+tests for 6-max_integer
+"""
+
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-class TestMaxInteger(unittest.TestCase):
-    def test_max_integer(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
-        self.assertEqual(max_integer([-1, -3, -4, -2]), -1)
-        self.assertEqual(max_integer([4]), 4)
-        self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([None]), None)
-        self.assertEqual(max_integer([1, 3, None, 4]), 4)
-        self.assertEqual(max_integer([3, 3, 3, 3]), 3)
 
-if __name__ == '__main__':
+class TestMaxInteger(unittest.TestCase):
+    """ tests for max_integer"""
+    def test_def(self):
+        """tests normal input"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_none(self):
+        """tests empty input"""
+        self.assertEqual(max_integer(), None)
+
+    def test_neg(self):
+        """tests negative"""
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+
+    def test_neg_pos(self):
+        """tests composed input"""
+        self.assertEqual(max_integer([1, -4, -5, 5, 3]), 5)
+
+    def test_non_int(self):
+        """tests with non integer input"""
+        with self.assertRaises(TypeError):
+            max_integer([1, '2', 3])
+
+    def test_float(self):
+        """tests with float"""
+        self.assertEqual(max_integer([1, 2, 3.3]), 3.3)
+
+if __name__ == "__main__":
     unittest.main()
