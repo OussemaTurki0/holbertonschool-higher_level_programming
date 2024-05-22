57aa58abe3b159018218115364bcbb5d39f05c21
OussemaTurki0 <oussematurki0@gmail.com>
2024-05-22 02:40:16 +0200

    commit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
 """
-Matrix Division Module
+
+Add Integer or Float Module
+
 """
 
-def matrix_divided(matrix, div):
+
+def add_integer(a, b=98):
     """
-    Divide all elements of the matrix by a divisor
+    ADD Two integer a and b
 
     Args:
-        matrix (list of lists of int/float): 2D list with rows of equal size
-        div (int/float): divisor
+        a (int/float): first int
+        b (int/float): Second int
 
     Raises:
-        TypeError: if matrix elements are not lists of integers/floats
-        TypeError: if rows of matrix are not the same size
-        TypeError: if div is not an integer or float
-        ZeroDivisionError: if div is zero
+        TypeError: in case the arguments are not int or float
 
-    Returns:
-        list of lists: new matrix with each element divided by div and rounded to 2 decimal places
+    Return:
+        (int) : Sum of the int a and b
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not all(isinstance(ele, (int, float)) for row in matrix for ele in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    return [[round(ele / div, 2) for ele in row] for row in matrix]
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    if type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+    if (a + b) == float('inf') or (a + b) == -float('inf'):
+        return b
+    return int(a) + int(b)
