a540a1efc74c52cea82c513968ff111a39bea108
OussemaTurki0 <oussematurki0@gmail.com>
2024-05-23 17:31:25 +0200

    changed

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,7 @@
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
