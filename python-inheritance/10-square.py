c3278531c5a89a0358fc1a5d688e5f96e686c7b4
OussemaTurki0 <oussematurki0@gmail.com>
2024-05-24 01:29:57 +0200

    changed

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.integer_validator("width", width)
