937259ce0d24ebe29a81833b715b982ee0f8024f
OussemaTurki0 <oussematurki0@gmail.com>
2024-05-30 00:20:03 +0200

    changed

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -14,6 +14,8 @@ def from_json_string(my_str):
     Converts a JSON string to a Python object.
 
     Args:
+        my_str (str): The JSON string to be converted.
+
     Returns:
         Any: The Python object corresponding to the JSON string.
     """
