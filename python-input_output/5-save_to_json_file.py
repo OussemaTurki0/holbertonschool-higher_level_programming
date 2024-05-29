49d6091dc10d1140460a77eccf9163b2cabc49a7
OussemaTurki0 <oussematurki0@gmail.com>
2024-05-30 00:20:13 +0200

    changed

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -13,7 +13,9 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """
-    Saves a Python ob
+    Saves a Python object to a file in JSON format.
+
+    Args:
         my_obj: The Python object to be saved.
         filename (str): The name of the file to save the object to.
 
