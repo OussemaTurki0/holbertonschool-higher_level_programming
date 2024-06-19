915464ad39c125c080a1fe00a1975ccb742ad7c8
oussematurki0 <oussematurki0@gmail.com>
2024-06-19 16:04:45 +0200

    commit

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- 7-cities.sql
+-- Create table cities with FOREIGN KEY constraint referencing states table
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
