b347c1b4d468cbafbba3d06e309b267b255a0ba4
oussematurki0 <oussematurki0@gmail.com>
2024-06-22 21:50:15 +0200

    commit

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,20 +5,28 @@ import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host='localhost',
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
 
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    cur.close()
-    db.close()
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+        cur.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQLdb Error:", e)
+        sys.exit(1)
